01c8f77e8b1d987f596ae8de57ec29178665fec5
ktoking <1550196694@qq.com>
2020-12-03 20:01:49 +0800

    12.3 分配资源

--- src/main/java/com/atguigu/wc/StreamWordCount.java
+++ src/main/java/com/atguigu/wc/StreamWordCount.java
@@ -12,9 +12,9 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         //创建流处理执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(8); //设置分布式多线程数
+        env.setParallelism(2); //设置分布式多线程数
         //从文件中读取路径
-//        String path="G:\\idea_repo\\FlinkTutorial\\src\\main\\resources\\hello.txt";
+//        String path="G:\\idea_repo\com.atguigu.wc.StreamWordCount\FlinkTutorial\\src\\main\\resources\\hello.txt";
 //        DataStream<String> inputDataStream = env.readTextFile(path);
 
         //从程序启动参数中提取配置项
@@ -27,8 +27,9 @@ public class StreamWordCount {
         //基于数据流转化计算
         DataStream<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
                 .keyBy(0)
-                .sum(1);
-        resultStream.print();
+                .sum(1).setParallelism(2);
+
+        resultStream.print().setParallelism(1);
 
         //执行任务 (有状态存储,每次进来一个流都会打印一次)
         env.execute();
